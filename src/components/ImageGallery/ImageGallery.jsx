6666bb7903427aaa4fe923011b227a73685031c2
Ira Panasiuk <panasykira2022@gmail.com>
2024-12-28 21:04:53 +0200

    update imageGalleryand imageCard

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,3 @@
-// import ImageCard from "../ImageCard/ImageCard";
-// import style from "./ImageGallery.module.css";
-
-// const ImageGallery = ({ images, onImageClick }) => {
-//   return (
-//     <ul className={style.galleryList}>
-//       {images.map((image) => (
-//         <li
-//           key={image.id}
-//           className={style.galleryItem}
-//           onClick={() => onImageClick(image.urls.regular)}
-//         >
-//           <ImageCard image={image} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-// export default ImageGallery;
-
 import ImageCard from '../ImageCard/ImageCard';
 import style from './ImageGallery.module.css';
 
